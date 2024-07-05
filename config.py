815ee87d2f3b16471a25dd09e7e6bede9b95becf
Abdullah Al Nahas <abdullah-alnahas@users.noreply.huggingface.co>
2024-07-05 08:23:26 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,12 @@ import logging
 from functools import lru_cache
 from typing import Union, Optional
 from pydantic_settings import BaseSettings
-from pydantic import SecretStr, PostgresDsn, DirectoryPath, Field, validator
+from pydantic import SecretStr, PostgresDsn, DirectoryPath, Field, AnyUrl, validator
 
 logger = logging.getLogger(__name__)
 
 class Settings(BaseSettings):
     
-    DATABASE_URL: PostgresDsn = Field(default="postgresql://mwk@localhost:5432/mwk")
     MAX_THREAD_NAME_LENGTH: int = Field(default=100)
 
     OPENAI_API_KEY: SecretStr
@@ -24,6 +23,18 @@ class Settings(BaseSettings):
     MAX_FUNCTION_TRIES: int = Field(default=3)
     MAX_FAILURES: int = Field(default=1)
     SYSTEM_PROMPT_FILE_NAME: str = Field(default="system_msg_fn")
+
+    # A/B Testing database connection configuration
+    AB_TESTING_DB_NAME: str
+    AB_TESTING_DB_USER: str
+    AB_TESTING_DB_PASSWORD: SecretStr
+    AB_TESTING_DB_HOST: AnyUrl
+    AB_TESTING_DB_PORT: int
+    
+    # A/B Testing environment variables
+    AB_TESTING_EXPERIMENT_ID: int
+    AB_TESTING_MODEL_1_ID: int
+    AB_TESTING_MODEL_2_ID: int
 
 @lru_cache()
 def get_settings() -> Settings:
